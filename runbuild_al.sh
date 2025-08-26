392adab53b89c89c1ef8b56e87801dd3de88c22e
Godis <mnl.manz@gmail.com>
2025-08-26 17:40:50 +0200

    alioth: update simple buildscript

--- runbuild_al.sh
+++ runbuild_al.sh
@@ -4,4 +4,11 @@ CONFIG_DIR="$SCRIPT_DIR/../arch/arm64/configs"
 cp "$CONFIG_DIR/nethunter_defconfig_alioth" "$CONFIG_DIR/nethunter_defconfig";
 git switch alioth;
 sudo ./build.sh
+if [ $? -eq 0 ]; then
+    git switch rls;
+    echo "Script completed successfully."
+else
+    echo "Script failed with exit code $?."
+fi
+
 
