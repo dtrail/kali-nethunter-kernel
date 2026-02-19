a0c802a40c6f6cc72421245a1af0288beedce58c
dtrail <mnl.manz@gmail.com>
2026-02-19 14:00:14 +0100

    Chimera Handler: Fix installer, debugging and service

--- Chimera_Module/customize.sh
+++ Chimera_Module/customize.sh
@@ -1,25 +1,33 @@
 #!/system/bin/sh
 
-# Standard-Variable: Sagt Magisk, dass wir selber entpacken wollen (oft sicherer)
-SKIPUNZIP=1
+# HINWEIS: Wir nutzen KEIN "SKIPUNZIP=1".
+# Magisk entpackt die ZIP automatisch nach $MODPATH, bevor dieses Skript startet.
 
-ui_print "- Installiere Chimera Familia Manager..."
+ui_print "- Installing Chimera Familia..."
 
-# 1. Entpacken
-# Wir entpacken alles aus der Zip in den Modul-Pfad ($MODPATH)
-unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
-unzip -o "$ZIPFILE" 'service.sh' -d $MODPATH >&2
-unzip -o "$ZIPFILE" 'system.prop' -d $MODPATH >&2
-unzip -o "$ZIPFILE" 'module.prop' -d $MODPATH >&2
+# 1. Alte Instanzen töten (Wie in deinem Beispiel)
+# Das verhindert "Text file busy" Fehler beim Überschreiben
+killall -9 chimera_controller.sh 2>/dev/null
+killall -9 chimera 2>/dev/null
 
-# 2. Berechtigungen setzen (CRITICAL!)
-ui_print "- Setze Ausführungsrechte..."
+# 2. Config Ordner erstellen
+ui_print "- Creating config directory..."
+mkdir -p /data/adb/chimera
 
-# Das CLI Tool
-set_perm $MODPATH/system/bin/chimera 0 0 0755
-# Der Controller
-set_perm $MODPATH/system/bin/chimera_controller.sh 0 0 0755
-# Der Autostart-Service
+# 3. Berechtigungen setzen (Das Wichtigste!)
+# Da Magisk schon entpackt hat, sind die Dateien jetzt in $MODPATH.
+ui_print "- Setting permissions..."
+
+# Alle Ordner auf 755, Dateien auf 644 (Basis-Rechte)
+# set_perm_recursive $MODPATH 0 0 0755 0644
+
+# Die Skripte ausführbar machen (755)
 set_perm $MODPATH/service.sh 0 0 0755
+set_perm $MODPATH/system/bin/chimera 0 0 0755
+set_perm $MODPATH/system/bin/chimera_controller.sh 0 0 0755
 
-ui_print "- Fertig! Rebooten."
+# 4. Aufräumen (Wie in deinem Beispiel)
+# customize.sh wird im installierten Modul nicht mehr gebraucht
+rm -f $MODPATH/customize.sh
+
+ui_print "- Installation successful! Please Reboot."
