76b2affed0012fc9cf8182043ec10a8d296ce873
Godis <mnl.manz@gmail.com>
2026-02-24 01:42:13 +0100

    Chimera handler: Complementary update for new blocklist logic

--- Chimera_Module/customize.sh
+++ Chimera_Module/customize.sh
@@ -1,33 +1,23 @@
 #!/system/bin/sh
+ui_print "- Installing Chimera Familia (v6.0 Profiles Edition)..."
 
-# HINWEIS: Wir nutzen KEIN "SKIPUNZIP=1".
-# Magisk entpackt die ZIP automatisch nach $MODPATH, bevor dieses Skript startet.
-
-ui_print "- Installing Chimera Familia..."
-
-# 1. Alte Instanzen töten (Wie in deinem Beispiel)
-# Das verhindert "Text file busy" Fehler beim Überschreiben
+# 1. Alte Instanzen stoppen
 killall -9 chimera_controller.sh 2>/dev/null
 killall -9 chimera 2>/dev/null
 
 # 2. Config Ordner erstellen
-ui_print "- Creating config directory..."
-mkdir -p /data/adb/chimera
+if [ ! -d "/data/adb/chimera" ]; then
+  ui_print "- Creating config directory..."
+  mkdir -p /data/adb/chimera
+fi
 
-# 3. Berechtigungen setzen (Das Wichtigste!)
-# Da Magisk schon entpackt hat, sind die Dateien jetzt in $MODPATH.
-ui_print "- Setting permissions..."
-
-# Alle Ordner auf 755, Dateien auf 644 (Basis-Rechte)
-# set_perm_recursive $MODPATH 0 0 0755 0644
-
-# Die Skripte ausführbar machen (755)
+# 3. Berechtigungen setzen
+ui_print "- Setting executable permissions..."
 set_perm $MODPATH/service.sh 0 0 0755
 set_perm $MODPATH/system/bin/chimera 0 0 0755
 set_perm $MODPATH/system/bin/chimera_controller.sh 0 0 0755
 
-# 4. Aufräumen (Wie in deinem Beispiel)
-# customize.sh wird im installierten Modul nicht mehr gebraucht
+# 4. Aufräumen
 rm -f $MODPATH/customize.sh
 
 ui_print "- Installation successful! Please Reboot."
